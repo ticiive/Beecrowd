dfc29f4202f88fe9e3006272c8562dde8c5646f2
ticiive <lelervlldsz4@gmail.com>
2026-02-27 21:56:55 -0300

    eu errei algumas vezes mas esqueci de add

---
+++ 1140.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string a,validador,caractere;
+
+    while (true)
+    {
+        getline(cin,a);
+        if (a == "*")
+        {
+            break;
+        }
+        
+        
+        for ( int i = 0; i < a.size(); i++)
+        {
+            if (a[i] == ' ')
+            {
+                if (toupper(a[i+1]) == toupper(a[0]))
+                {
+                    validador = "Y";
+                }else{
+                    validador = "N";
+                }
+                
+            }
+            if (i==a.size()-1)
+            {
+                cout<<validador<<"\n";
+            }
+            
+            
+        }
+        
+    }
+    
+
+
+
+
+
+
+}
