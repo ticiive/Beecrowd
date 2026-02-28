33530ed7e1ac4c2f864978be65c8e5b1ad439598
ticiive <lelervlldsz4@gmail.com>
2026-02-28 02:14:53 -0300

    runtime error :)

--- 1245.cpp
+++ 1245.cpp
@@ -1,43 +1,90 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(){
-    int n,contador;
-    char c;
-    string lados;
+
+    int n,tam,aux,contador,tame,tamd;
+    char lado;
+    vector<int> e,d;
 
 
 while (cin>>n)
 {
-    int tamanhos[n];
-    lados.clear();
     contador = 0;
+    tame=0,tamd=0;
+    e.clear();d.clear();
+
     for (int i = 0; i < n; i++)
     {
-        cin>>tamanhos[i]>>c;
-        lados += c;
+        cin>>tam>>lado;
+        if (lado == 'E')
+        {
+            e.push_back(tam);
+            tame++;
 
+        }else{
+            d.push_back(tam);
+            tamd++;
+        }
     }
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++) // para evitar contar duplamente
+        for (int j = 0; j < n; j++)
         {
 
-            
-            if (tamanhos[i] == tamanhos[j] && lados[i] != lados[j])
+            if (i>= tamd && i>= tame)
             {
-              tamanhos[i] = 3*j;
-              tamanhos[j] = 4*i;
-                contador++;
+                break;
+            }
+            
+            if (i<tame)
+            {
+                if (e[j]>e[i])
+                {
+                aux = e[j];
+                e[j] = e[i];
+                e[i] = aux;
+                }
+                
+
+            }
+            if (i<tamd)
+            {
+                if (d[j]>d[i])
+                {
+                aux = d[j];
+                d[j] = d[i];
+                d[i] = aux;
+                }
+                
+            }
+
+            
+            
+        }
+    }
+    while (!e.empty() && !d.empty())
+    {
+        
+        if (d[0]==e[0])
+        {
+            contador++;
+            e.erase(e.begin());
+            d.erase(d.begin());
+        }else{
+            if (d[0]>e[0])
+            {
+                e.erase(e.begin());
+            }else{
+                d.erase(d.begin());
             }
             
         }
         
     }
-    cout<<contador/2<<"\n";
-    
-    
-}
+    cout<<contador<<"\n";
 
+}
 }
