3b0ac516ce85fe19500a586709ba834d081e3c15
ticiive <lelervlldsz4@gmail.com>
2026-03-10 22:06:42 -0300

    timelimit rs

---
+++ 1151.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int fib(int x){
+    if (x == 0)
+    {
+        return 0;
+    }
+    if (x == 1)
+    {
+        return 1;
+    }
+
+    return fib(x-1)+fib(x-2);
+    
+    
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<fib(n);
+    }
+    
+
+
+}
